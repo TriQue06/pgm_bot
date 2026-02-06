f732f6ecca769b5f4713070e6dab88fb6f3f85ee
eren-za <eren-za@users.noreplygithub.com>
2026-02-06 22:57:47 +0300

    Create bilgi.js

---
+++ commands/general/bilgi.js
@@ -0,0 +1,41 @@
+module.exports = {
+    name: "bilgi",
+    description: "PGM Bot hakkında bilgi verir",
+
+    execute(message) {
+        message.channel.send(`
+        **🤖 PGM Bot | Bilgi Menüsü**
+
+        Merhaba! Ben **PGM Bot**
+        Sunucunu daha eğlenceli, düzenli ve rekabetçi hale getirmek için buradayım.
+
+        ━━━━━━━━━━━━━━━━━━
+        📊 **Genel Bilgiler**
+        • Bot Adı: **PGM Bot**
+        • Sürüm: **v0.38.0 Beta**
+        • Geliştirici: **BayPGM & Eros**
+        • Durum: **Çevrimiçi**
+
+        ━━━━━━━━━━━━━━━━━━
+        💰 **Ekonomi Sistemi**
+        • <:pgmcoin:> PGM Coin
+        • 🛒 Market & Kitler
+        • 🎁 Günlük ödüller
+
+        ━━━━━━━━━━━━━━━━━━
+        ⚔️ **Eğlence**
+        • 🎲 Mini oyunlar
+        • 🧩 Şans sistemleri
+
+        ━━━━━━━━━━━━━━━━━━
+        📖 **Komutlar**
+        • !yardim
+        • !market
+        • !ping
+        • !pgmyisik
+
+        ━━━━━━━━━━━━━━━━━━
+        ✨ **PGM Bot – Premium Bot**
+        `);
+    }
+};
